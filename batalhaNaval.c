3f02fae5dbc3569790bfac2a39da355c01bbeea3
Matheus Moura <paulomatheus8933@gmail.com>
2025-05-06 17:40:11 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
+
 // Cria o tabuleiro
 int main() {
-    int tabuleiro[5][5] = {0}; 
-    // Estruturas for com a posição
-    for (int i = 0; i < 3; i++)
+    int tabuleiro[10][10] = {0}; 
+
+// Estrutura for com a posição dos navios 
+    for (int i = 4; i <= 7; i++)
     {
-        tabuleiro[1][i] = 1;
-        printf("Navio horizontal: (%d, %d)\n", 1, i);
+        tabuleiro[2][i] = 3;
+    }
+
+    for (int i = 5; i <= 8; i++)
+    {
+        tabuleiro[i][1] = 3;
     }
 
     for (int i = 0; i < 4; i++)
     {
-        tabuleiro[i][3] = 2;
-        printf("Navio vertical: (%d, %d)\n", i, 3);
+        tabuleiro[i][i] = 3;
+    }
+
+    for (int i = 0; i < 4; i++) 
+    {
+        tabuleiro[i][9 - i] = 3;
+    }
+
+// Exibe as informações dos navios
+    printf("\nTabuleiro:\n");
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+        
     }
     
+
     return 0;
 }
